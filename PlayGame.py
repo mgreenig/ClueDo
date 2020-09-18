596e5a37a883b154d5161e69227db3c4ae2361b5
Matt <matt.greenig@gmail.com>
2020-09-18 17:53:52 +0100

    added if __name__ == '__main__'

--- PlayGame.py
+++ PlayGame.py
@@ -1,50 +1,52 @@
 import ClueDo
 from PyInquirer import prompt
 
-# input the players participating in the game
-players = prompt([{
-    'type': 'checkbox',
-    'name': 'players',
-    'message': 'Which characters are playing?',
-    'choices': [{'name': char} for char in ClueDo.ClueGame.characters],
-}])
+if __name__ == '__main__':
 
-# ensure that 3 or more players are selected
-while len(players['players']) < 3:
+    # input the players participating in the game
     players = prompt([{
         'type': 'checkbox',
         'name': 'players',
-        'message': 'Please choose between 3 and 6 characters',
+        'message': 'Which characters are playing?',
         'choices': [{'name': char} for char in ClueDo.ClueGame.characters],
     }])
 
-# input which character is to be controlled by the computer
-my_char = prompt([{
-    'type': 'list',
-    'name': 'my_char',
-    'message': 'Which character will I be playing?',
-    'choices': [{'name': char} for char in players['players']]
-}])
+    # ensure that 3 or more players are selected
+    while len(players['players']) < 3:
+        players = prompt([{
+            'type': 'checkbox',
+            'name': 'players',
+            'message': 'Please choose between 3 and 6 characters',
+            'choices': [{'name': char} for char in ClueDo.ClueGame.characters],
+        }])
 
-# input the cards to be used by the computer
-my_cards = prompt([{
-    'type': 'checkbox',
-    'name': 'my_cards',
-    'message': 'What are my cards?',
-    'choices': [{'name': char} for char in ClueDo.ClueGame.all_cards]
-}])
+    # input which character is to be controlled by the computer
+    my_char = prompt([{
+        'type': 'list',
+        'name': 'my_char',
+        'message': 'Which character will I be playing?',
+        'choices': [{'name': char} for char in players['players']]
+    }])
 
-# ensure that exactly 3 cards are inputted
-while len(my_cards['my_cards']) != 3:
+    # input the cards to be used by the computer
     my_cards = prompt([{
         'type': 'checkbox',
         'name': 'my_cards',
-        'message': 'Please select exactly 3 cards',
+        'message': 'What are my cards?',
         'choices': [{'name': char} for char in ClueDo.ClueGame.all_cards]
     }])
 
-# initialise instance of the class
-game_instance = ClueDo.ClueGame(players['players'], my_char['my_char'], my_cards['my_cards'])
+    # ensure that exactly 3 cards are inputted
+    while len(my_cards['my_cards']) != 3:
+        my_cards = prompt([{
+            'type': 'checkbox',
+            'name': 'my_cards',
+            'message': 'Please select exactly 3 cards',
+            'choices': [{'name': char} for char in ClueDo.ClueGame.all_cards]
+        }])
 
-# begin the game
-game_instance.start_game()
+    # initialise instance of the class
+    game_instance = ClueDo.ClueGame(players['players'], my_char['my_char'], my_cards['my_cards'])
+
+    # begin the game
+    game_instance.start_game()
